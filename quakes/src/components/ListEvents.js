8cddff10332cc86a800c1e6f2cd959924052d24f
rhockr <hola@rholo.cl>
2018-06-12 12:25:32 -0400

    test components

--- quakes/src/components/ListEvents.js
+++ quakes/src/components/ListEvents.js
@@ -4,6 +4,11 @@ export default class ListEvents extends React.Component{
     render(){
         return(
             <li>3.4 en Valparaiso</li>
+            <li>
+                <a href="#">
+                    4.4 Valparaiso a las 13:30 Hrs.
+                </a>
+            </li>
         );
     }
 }
--- quakes/src/screen/QuakeList.js
+++ quakes/src/screen/QuakeList.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ListEvents from 'components/ListEvents';
 
+const state = {
+    quakes:[]
+}
+
 export default class QuakeList extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            quakes:[]
+        }
+    }
+    componentDidMount(){
+        this.setState({quakes:quakes})
+    }
     render(){
         return (
             <ul>
-                <ListEvents />
+                <ListEvents events={this.state.quakes} />
             </ul>
         );
     }
